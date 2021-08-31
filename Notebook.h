e428fb4f09f2ade7d3f2428f9c79326d7e8dfb75
jsaunders25 <86677482+jsaunders25@users.noreply.github.com>
2021-08-31 15:49:13 -0400

    Update Notebook.h
    Organizing and commenting
    Added preferences as properties

--- Notebook.h
+++ Notebook.h
@@ -16,50 +16,58 @@ class Notebook : public QMainWindow
     Q_OBJECT
 
 public:
-    Notebook(QWidget *parent = nullptr);
+    Notebook(QString open_file, bool new_file, QWidget *parent = nullptr);
     ~Notebook();
 
 protected:
     void closeEvent(QCloseEvent *event);
 private slots:
-    // Menu Actions
+    // File Menu Actions
+    void on_actionNew_triggered();
+    void on_actionNew_Window_triggered();
     void on_actionOpen_triggered();
+    void on_actionSave_triggered();
+    void on_actionSave_As_triggered();
+    void on_actionPrint_triggered();
     void on_actionExit_triggered();
+
+    // Edit Menu Actions
     void on_actionCopy_triggered();
     void on_actionPaste_triggered();
     void on_actionCut_triggered();
     void on_actionUndo_triggered();
     void on_actionRedo_triggered();
-    void on_actionSave_triggered();
-    void on_actionSave_As_triggered();
-    void on_actionPrint_triggered();
-    void on_actionNew_triggered();
+    void on_actionFind_triggered();
+    void on_actionReplace_triggered();
 
-    // Editor Slots
+    // Tools Menu Actions
+    void on_actionPreferences_triggered();
+    void updatePreferences(bool load_recent, bool word_wrap, bool status_bar);
+    void on_actionFont_triggered();
+    void fontChanged(QFont font);
+
+    // Find/Replace Slots
     void findNext(QString text);
     void findPrev(QString text);
     void replace(QString original, QString target);
     void replaceAll(QString original, QString target);
 
-    void clipboardChanged();
-
+    // Text Edit Slots
     void on_textEdit_redoAvailable(bool b);
     void on_textEdit_undoAvailable(bool b);
     void on_textEdit_copyAvailable(bool b);
+    void clipboardChanged();
 
-    void on_actionFind_triggered();
-    void on_actionReplace_triggered();
-
-    void on_actionFont_triggered();
-    void fontChanged(QFont font);
+    // Status Bar Slots
+    void cursorMoved();
 
 private:
     Ui::Notebook *ui;
     FindDialog  *find_dialog;
-    QClipboard  *clipboard;     // window system clipboard
-    QString     m_current_filename; // only name of current file
-    QString     m_current_file_path; // full path of current file
-    QString     m_current_dir;  // directory of current file
+    QClipboard  *clipboard; /**< window system clipboard */
+    QString     m_current_filename; /**< only name of current file */
+    QString     m_current_file_path; /**< full path of current file */
+    QString     m_current_dir;  /**< directory of current file */
 
     void openFileDialog();
     void newFile();
@@ -69,5 +77,8 @@ private:
     void print();
 
     void setCurrentFile(QFile *file);
+
+    // Preferences
+    bool m_load_recent, m_word_wrap, m_status_bar;
 };
 #endif // NOTEBOOK_H
